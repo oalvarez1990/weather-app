5d924191a8d31e387cb2048bf8f62a60d41567c3
oalvarez1990 <omaraleyser@hotmail.com>
2022-03-09 14:18:12 -0500

    Creacion de app estado de clima

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Weather from './components/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+              
+      </header> */}
+
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,60 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+    const [weather, setWeather] = useState({})
+    const [change, setchange] = useState(true);
+    const [temperature, setAssigntemperature] = useState(0)
+
+    const success = pos => {
+        const lat = pos.coords.latitude;
+        const lon = pos.coords.longitude;
+        console.log(lat,lon)
+
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6897765b297b912b5d59c01a10df8a8b`)
+            .then(res => setWeather(res.data));
+
+    }
+    console.log(weather)
+
+    const convert = () => {
+        if (change) {
+            const far = (weather.main?.temp - 273.15) * (9 / 5) + 32;
+            setAssigntemperature(far.toFixed(2))
+            setchange(false)
+
+        } else {
+            const degrees = (weather.main?.temp) - (273.15)
+            setAssigntemperature(degrees.toFixed(2))
+            setchange(true)
+        }
+    }
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(success);
+    }, []);
+
+
+    return (
+        <div className="App">            
+            <span className="humidity"> {weather.main?.humidity}</span>
+            <p className="name">{weather.name},{weather.sys?.country}.</p>
+            <h1 className="temp_max">{weather.main?.clouds}</h1><br />         
+            <h2 className="temp_max">{weather.wind?.speed}</h2><br />        
+            
+            <span className="temperature">{temperature}</span> 
+            <span className="temperature">{weather.coords?.lat}</span> 
+
+            <div className="button">
+                <button onClick={convert}>Chance weather</button>
+            </div>
+
+            <img className='icon' src={weather.weather?.[0].icon ? `http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png` : ""} alt="" />
+        </div>
+
+    )
+
+}
+export default Weather;
